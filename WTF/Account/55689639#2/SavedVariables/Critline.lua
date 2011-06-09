d601593e72a4a1fd4f62d82755c493be8afaafcb
elliottcable <me@ell.io>
2011-06-09 03:57:09 -0400

    (- new config) Initial Critline settings

---
+++ WTF/Account/55689639#2/SavedVariables/Critline.lua
@@ -0,0 +1,22 @@
+
+CritlineDB = {
+	["namespaces"] = {
+		["splash"] = {
+		},
+		["LibDualSpec-1.0"] = {
+		},
+		["display"] = {
+		},
+		["filters"] = {
+		},
+		["minimap"] = {
+		},
+	},
+	["profileKeys"] = {
+		["Battlecollie - Ysondre"] = "Battlecollie - Ysondre",
+	},
+	["profiles"] = {
+		["Battlecollie - Ysondre"] = {
+		},
+	},
+}
---
+++ WTF/Account/55689639#2/Ysondre/Battlecollie/SavedVariables/Critline.lua
@@ -0,0 +1,14 @@
+
+CritlinePerCharDB = {
+	["namespaces"] = {
+		["LibDualSpec-1.0"] = {
+		},
+	},
+	["profileKeys"] = {
+		["Battlecollie - Ysondre"] = "Battlecollie - Ysondre",
+	},
+	["profiles"] = {
+		["Battlecollie - Ysondre"] = {
+		},
+	},
+}
